98096018c2d28c1f95ff3e084750d5e1e750fca1
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-06-25 15:15:10 +0800

    refactor(splunk): merge shared codes# <type>: (If applied, this commit will...) <subject> (Max 50 char)

--- installer/splunk.sh
+++ installer/splunk.sh
@@ -14,8 +14,8 @@ if [[ $SYSTEM != *"Linux"* ]]; then
 fi
 
 # determine which package type should download based on which distro is running on
-# Ubuntu -> deb
-# centos -> rpm
+# Ubuntu/debian -> deb
+# centos/RHEL -> rpm
 # others -> tgz
 DISTRO=$(cat /etc/*-release)
 PACKAGE_TYPE='tgz'
@@ -27,11 +27,21 @@ elif [[ $DISTRO == *"CentOS"* ]]; then
   PACKAGE_TYPE='rpm'
 fi
 
-# determine whether dependencies are available
-WGET = $(which wget)
-CURL = $(which wget)
-
-clear
+# make the "en_US.UTF-8" locale so splunk will be utf-8 enabled by default
+if [ $PACKAGE_TYPE == "deb" ] ; then
+    apt-get update
+    apt-get install -y apt-utils dialog locales procps wget curl
+    echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+    locale-gen en_US.UTF-8
+    export LANGUAGE="en_US.UTF-8"
+    export LANG="en_US.UTF-8"
+elif [ $PACKAGE_TYPE == "rpm" ] ; then
+    yum install wget curl which -y
+    localedef -c -f UTF-8 -i en_US en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
+fi
 
 export SPLUNK_HOME="/opt/splunk"
 SPLUNK_BACKUP_DEFAULT_ETC="/var/opt/splunk"
@@ -43,126 +53,56 @@ SPLUNK_USER="splunk"
 groupadd -r ${SPLUNK_GROUP} 
 useradd -r -m -g ${SPLUNK_GROUP} ${SPLUNK_USER}
 
+SPLUNK_URL=$(curl -s https://www.splunk.com/en_us/download/splunk-enterprise.html | grep -o "https[^\"]*.${PACKAGE_TYPE}" | head -n 1)
+SPLUNK_FILENAME=${SPLUNK_URL##*/}
+mkdir -p ${SPLUNK_HOME}
+# wget -O /tmp/${SPLUNK_FILENAME} ${SPLUNK_URL}
 if [ $PACKAGE_TYPE == "deb" ] ; then
-    # env setup
-    ## make the "en_US.UTF-8" locale so splunk will be utf-8 enabled by default
-    apt-get update
-    apt-get install -y --no-install-recommends apt-utils dialog locales procps
-    echo "LC_ALL=en_US.UTF-8" >> /etc/environment
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-    echo "LANG=en_US.UTF-8" > /etc/locale.conf
-    locale-gen en_US.UTF-8
-    export LANGUAGE="en_US.UTF-8"
-    export LANG="en_US.UTF-8"
+    cp /opt/test.deb /tmp/${SPLUNK_FILENAME}
+elif [ $PACKAGE_TYPE == "rpm" ] ; then
+    cp /opt/test.rpm /tmp/${SPLUNK_FILENAME}
+fi
 
-    ## install tools needed in the setup if necessary
-    if [ -z "$WGET" ]; then
-        apt-get install -y wget
-    fi
-    if [ -z "$CURL" ]; then
-        apt-get install -y curl
-    fi
+wget -O /tmp/${SPLUNK_FILENAME}.md5 ${SPLUNK_URL}.md5
+cd /tmp
+if [ "$(md5sum ${SPLUNK_FILENAME} | awk '{print $1}')" != "$(cat ${SPLUNK_FILENAME}.md5 | awk '{print $NF}')" ]; then
+    echo "The MD5 of downloaded file is different from expected, please re-run the script to download it again."
+    exit 1
+fi
 
-    SPLUNK_URL=$(curl -s https://www.splunk.com/en_us/download/splunk-enterprise.html | grep -o "https[^\"]*.${PACKAGE_TYPE}" | head -n 1)
-    SPLUNK_FILENAME=${SPLUNK_URL##*/}
-    mkdir -p ${SPLUNK_HOME}
-    wget -O /tmp/${SPLUNK_FILENAME} ${SPLUNK_URL}
-    # cp /opt/test.deb /tmp/${SPLUNK_FILENAME}
-    wget -O /tmp/${SPLUNK_FILENAME}.md5 ${SPLUNK_URL}.md5
-    cd /tmp
-    if [ "$(md5sum ${SPLUNK_FILENAME} | awk '{print $1}')" != "$(cat ${SPLUNK_FILENAME}.md5 | awk '{print $NF}')" ]; then
-        echo "The MD5 of downloaded file is different from expected, please re-run the script to download it again."
-        exit 1
-    fi
-
+if [ $PACKAGE_TYPE == "deb" ] ; then
     dpkg -i ${SPLUNK_FILENAME}
 
-    ## post installation
-    rm /tmp/${SPLUNK_FILENAME} 
-    rm /tmp/${SPLUNK_FILENAME}.md5 
-    mkdir -p /var/opt/splunk 
-    cp -R ${SPLUNK_HOME}/etc ${SPLUNK_BACKUP_DEFAULT_ETC}
-    # echo "OPTIMISTIC_ABOUT_FILE_LOCKING=1" >> ${SPLUNK_HOME}/etc/splunk-launch.conf
-
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/etc
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_BACKUP_DEFAULT_ETC}
-    # remove tools that used in the setup if necessary
-    if [ -z "$WGET" ]; then
-        apt-get purge -y --auto-remove wget
-    fi
-    if [ -z "$CURL" ]; then
-        apt-get purge -y --auto-remove curl
-    fi
-
-    # filesystem locktest will fail on docker, it needs to be disabled on docker
-    CONTROL_GROUPS=$(cat /proc/1/cgroup)
-    if [[ $CONTROL_GROUPS == *"docker"* ]]; then
-        echo "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" >> ${SPLUNK_HOME}/etc/splunk-launch.conf
-    fi
-
-    ${SPLUNK_HOME}/bin/splunk enable boot-start --accept-license
-    ${SPLUNK_HOME}/bin/splunk start
-    
     # write variables to shell profile
     echo "export LANGUAGE=\"en_US.UTF-8\"
     export LANG=\"en_US.UTF-8\"
     export SPLUNK_HOME=/opt/splunk">>~/.bash_profile
-
 elif [ $PACKAGE_TYPE == "rpm" ] ; then
-    localedef -c -f UTF-8 -i en_US en_US.UTF-8
-    export LC_ALL=en_US.UTF-8
-
-    if [ -z "$WGET" ]; then
-        yum install wget -y
-    fi
-    if [ -z "$CURL" ]; then
-        yum install curl -y
-    fi
-
-    SPLUNK_URL=$(curl -s https://www.splunk.com/en_us/download/splunk-enterprise.html | grep -o "https[^\"]*.${PACKAGE_TYPE}" | head -n 1)
-    SPLUNK_FILENAME=${SPLUNK_URL##*/}
-    mkdir -p ${SPLUNK_HOME}
-    wget -O /tmp/${SPLUNK_FILENAME} ${SPLUNK_URL}
-    # cp /opt/test.rpm /tmp/${SPLUNK_FILENAME}
-    wget -O /tmp/${SPLUNK_FILENAME}.md5 ${SPLUNK_URL}.md5
-    cd /tmp
-    if [ "$(md5sum ${SPLUNK_FILENAME} | awk '{print $1}')" != "$(cat ${SPLUNK_FILENAME}.md5 | awk '{print $NF}')" ]; then
-        echo "The MD5 of downloaded file is different from expected, please re-run the script to download it again."
-        exit 1
-    fi
-
     rpm -i ${SPLUNK_FILENAME}
 
-    ## post installation
-    rm /tmp/${SPLUNK_FILENAME} 
-    rm /tmp/${SPLUNK_FILENAME}.md5 
-    mkdir -p /var/opt/splunk 
-    cp -R ${SPLUNK_HOME}/etc ${SPLUNK_BACKUP_DEFAULT_ETC}
-    # echo "OPTIMISTIC_ABOUT_FILE_LOCKING=1" >> ${SPLUNK_HOME}/etc/splunk-launch.conf
-
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/etc
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_BACKUP_DEFAULT_ETC}
-    # remove tools that used in the setup if necessary
-    if [ -z "$WGET" ]; then
-        yum erase wget -y
-    fi
-    if [ -z "$CURL" ]; then
-        yum erase curl -y
-    fi
-
-    # filesystem locktest will fail on docker, it needs to be disabled on docker
-    CONTROL_GROUPS=$(cat /proc/1/cgroup)
-    if [[ $CONTROL_GROUPS == *"docker"* ]]; then
-        echo "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" >> ${SPLUNK_HOME}/etc/splunk-launch.conf
-    fi
-
-    ${SPLUNK_HOME}/bin/splunk enable boot-start --accept-license --no-prompt
-    ${SPLUNK_HOME}/bin/splunk start 
-    
     # write variables to shell profile
     echo "export LC_ALL=en_US.UTF-8
-    export SPLUNK_HOME=/opt/splunk">>~/.bash_profile
-    
+    export SPLUNK_HOME=/opt/splunk">>~/.bash_profile 
 fi
+
+## post installation
+rm /tmp/${SPLUNK_FILENAME} 
+rm /tmp/${SPLUNK_FILENAME}.md5 
+mkdir -p /var/opt/splunk 
+cp -R ${SPLUNK_HOME}/etc ${SPLUNK_BACKUP_DEFAULT_ETC}
+
+chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}
+chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/etc
+chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_BACKUP_DEFAULT_ETC}
+
+# default user admin/admin
+echo "[user_info]
+USERNAME = admin
+PASSWORD = admin" > ${SPLUNK_HOME}/etc/system/local/user-seed.conf
+${SPLUNK_HOME}/bin/splunk enable boot-start --accept-license --no-prompt
+# filesystem locktest will fail on docker, it needs to be disabled on docker
+CONTROL_GROUPS=$(cat /proc/1/cgroup)
+if [[ $CONTROL_GROUPS == *"docker"* ]]; then
+    echo "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" >> ${SPLUNK_HOME}/etc/splunk-launch.conf
+fi
+${SPLUNK_HOME}/bin/splunk start
